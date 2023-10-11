491070cda0bf8a6a4662aee7c078cc15dae98e39
Nathan HANEN <hanen.nathan@gmail.com>
2023-10-11 08:59:39 +0200

    only applies to hotbar or backpack inventories

--- OfflineFoodNoSpoil/OfflineFoodNoSpoil.cs
+++ OfflineFoodNoSpoil/OfflineFoodNoSpoil.cs
@@ -29,45 +29,50 @@ namespace Wiltoga
 
         private void Event_PlayerDisconnect(IServerPlayer byPlayer)
         {
-            foreach (var slot in byPlayer.InventoryManager.Inventories.Values.SelectMany(o => o))
+            foreach (var inventory in byPlayer.InventoryManager.Inventories.Values)
             {
-                if (slot.Itemstack is not null)
+                if (inventory.ClassName != "hotbar" && inventory.ClassName != "backpack")
+                    continue;
+                foreach (var slot in inventory)
                 {
-                    var stack = slot.Itemstack;
-                    var freshAttribute = (stack.Attributes["transitionstate"] as ITreeAttribute)?["freshHours"] as FloatArrayAttribute;
-                    if (freshAttribute is null)
-                        continue;
-                    var currentFreshness = freshAttribute.value[0];
-                    Server.WorldManager.SaveGame.StoreData($"{byPlayer.PlayerUID}.{slot.Inventory.InventoryID}.{slot.Inventory.GetSlotId(slot)}.{DataOldFoodSpoil}", currentFreshness);
-                    freshAttribute.value[0] = float.MaxValue;
-                    slot.MarkDirty();
+                    if (slot.Itemstack is not null)
+                    {
+                        var stack = slot.Itemstack;
+                        var freshAttribute = (stack.Attributes["transitionstate"] as ITreeAttribute)?["freshHours"] as FloatArrayAttribute;
+                        if (freshAttribute is null)
+                            continue;
+                        var currentFreshness = freshAttribute.value[0];
+                        Server.WorldManager.SaveGame.StoreData($"{byPlayer.PlayerUID}.{slot.Inventory.InventoryID}.{slot.Inventory.GetSlotId(slot)}.{DataOldFoodSpoil}", currentFreshness);
+                        freshAttribute.value[0] = float.MaxValue;
+                        slot.MarkDirty();
+                    }
                 }
             }
         }
 
         private void Event_PlayerJoin(IServerPlayer byPlayer)
         {
+            foreach (var inventory in byPlayer.InventoryManager.Inventories.Values)
             {
-                foreach (var inventory in byPlayer.InventoryManager.Inventories.Values)
+                if (inventory.ClassName != "hotbar" && inventory.ClassName != "backpack")
+                    continue;
+                foreach (var slot in inventory)
                 {
-                    foreach (var slot in inventory)
+                    if (slot.Itemstack is not null)
                     {
-                        if (slot.Itemstack is not null)
+                        var stack = slot.Itemstack;
+                        var freshAttribute = (stack.Attributes["transitionstate"] as ITreeAttribute)?["freshHours"] as FloatArrayAttribute;
+                        var createdTotalHours = (stack.Attributes["transitionstate"] as ITreeAttribute)?["createdTotalHours"] as DoubleAttribute;
+                        var lastUpdatedTotalHours = (stack.Attributes["transitionstate"] as ITreeAttribute)?["lastUpdatedTotalHours"] as DoubleAttribute;
+                        if (freshAttribute is null || createdTotalHours is null || lastUpdatedTotalHours is null)
+                            continue;
+                        var hours = Server.World.Calendar.TotalHours;
+                        var savedFreshness = Server.WorldManager.SaveGame.GetData($"{byPlayer.PlayerUID}.{slot.Inventory.InventoryID}.{slot.Inventory.GetSlotId(slot)}.{DataOldFoodSpoil}", float.MinValue);
+                        if (savedFreshness >= 0)
                         {
-                            var stack = slot.Itemstack;
-                            var freshAttribute = (stack.Attributes["transitionstate"] as ITreeAttribute)?["freshHours"] as FloatArrayAttribute;
-                            var createdTotalHours = (stack.Attributes["transitionstate"] as ITreeAttribute)?["createdTotalHours"] as DoubleAttribute;
-                            var lastUpdatedTotalHours = (stack.Attributes["transitionstate"] as ITreeAttribute)?["lastUpdatedTotalHours"] as DoubleAttribute;
-                            if (freshAttribute is null)
-                                continue;
-                            var hours = Server.World.Calendar.TotalHours;
-                            var savedFreshness = Server.WorldManager.SaveGame.GetData($"{byPlayer.PlayerUID}.{slot.Inventory.InventoryID}.{slot.Inventory.GetSlotId(slot)}.{DataOldFoodSpoil}", float.MinValue);
-                            if (savedFreshness >= 0)
-                            {
-                                createdTotalHours.value = lastUpdatedTotalHours.value = hours;
-                                freshAttribute.value[0] = savedFreshness;
-                                slot.MarkDirty();
-                            }
+                            createdTotalHours.value = lastUpdatedTotalHours.value = hours;
+                            freshAttribute.value[0] = savedFreshness;
+                            slot.MarkDirty();
                         }
                     }
                 }
